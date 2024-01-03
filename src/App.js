f0126809b3ce500a916b813d9f2a65e8637370c7
Shomyakumar <shomyakumar90@gmail.com>
2024-01-03 21:14:18 +0530

    added default route

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Navbar from "./components/Navbar"
-
+import { Navigate } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Routes>
+      <Routes >
         <Route path="/" element={ <Home/> } />
         <Route path="/about" element={ <About/> } />
         <Route path="/contact" element={ <Contact/> } />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
   )
