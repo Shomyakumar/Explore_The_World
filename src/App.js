bb348a4d6919595d1c5051d5ad59de3a1b49f269
Shomyakumar <shomyakumar90@gmail.com>
2024-01-05 15:08:00 +0530

    changed routes

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
     <div className="App">
       <Navbar/>
       <Routes >
+        <Route path="/" element={ <Home/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="/contact" element={ <Contact/> } />
         <Route path="/Explore_The_World" element={ <Home/> } />
-        <Route path="/Explore_The_World/about" element={ <About/> } />
-        <Route path="/Explore_The_World/contact" element={ <Contact/> } />
-        
       </Routes>
     </div>
   )
