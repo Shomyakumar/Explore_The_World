6a406ef968894afd1285d0c9b069728a597271be
Shomyakumar <shomyakumar90@gmail.com>
2024-01-12 23:32:25 +0530

    link added to icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 
 import insta from "../assets/insta.png";
-import twitter from "../assets/twitter.png"
 import linkedin from "../assets/linkedIn.png"
-
+import github from "../assets/github.png"
+import { Link } from "react-router-dom";
 import Logo from "../assets/logo2.png"
 export default function Footer(){
     return(
@@ -21,9 +21,15 @@ export default function Footer(){
                 <div className="flex flex-col gap-4">
                     <p className="text-white font-semibold text-lg">Follow me on</p>
                     <div className="flex gap-4">
-                        <button ><img   className="w-8" src={insta} alt="insta"/></button>
-                        <button ><img   className="w-8" src={twitter} alt="twitter"/></button>
-                        <button ><img   className="w-8" src={linkedin} alt="linkedIn"/></button>
+                        <Link to="https://www.instagram.com/invites/contact/?i=1f6eifg9px192&utm_content=fkhuuwn">
+                            <img   className="w-8" src={insta} alt="insta"/>
+                        </Link>
+                        <Link to="https://www.linkedin.com/in/shomya-kumar-9b06a7220" >
+                            <img   className="w-8" src={linkedin} alt="linkedin"/>
+                        </Link>
+                        <Link to="https://github.com/Shomyakumar" >
+                            <img   className="w-8 bg-white rounded-full p-[1px]" src={github} alt="github"/>
+                        </Link>
                     </div>
                 </div>
             </div>
